e450e08777241f18322673efe4df1483a877079c
Gage Norton <nortongage0@gmail.com>
2023-03-09 11:55:21 -0600

    AddRole partial function

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 let inProgress = true;
 
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    database: 'team_db',
+    password: 'steph'
+});
+
 const firstQuestion = [
     {
         name: 'action',
@@ -19,18 +26,40 @@ const newDepartment = [
     }
 ];
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    database: 'team_db',
-    password: 'steph'
-});
+const newRole = [
+    {
+        name: 'title',
+        message: "What's the title of the new role?",
+        type: 'input'
+    },
+    {
+        name: 'salary',
+        message: "What is the salary for this position?",
+        type: 'input'
+    },
+    {
+        name: 'depID',
+        message: "What the department this role belongs to?",
+        type: 'list',
+        choices: []
+    }
+];
 
 async function startApplication() {
     let inProgress = true;
     while (inProgress) {
         const answers = await inquirer.prompt(firstQuestion)
         switch (answers.action) {
+            case 'Add Role':
+                const newRoleQuestion = await inquirer.prompt(newRole)
+                const roleTitle = newRoleQuestion.title;
+                const roleSalary = newRoleQuestion.salary;
+                const roleID = newRoleQuestion.depID;
+                connection.query(`INSERT INTO role (title, salary, department_id) VALUES ('${roleTitle, roleSalary,roleID}')`, function (err, results) {
+                    console.log(`\n\n ${roleTitle} role was added!\n`);
+                    startApplication()
+                });
+                break;
             case 'Add Department':
                 const newDepQuestion = await inquirer.prompt(newDepartment)
                 const newDep = newDepQuestion.newDepartment;
